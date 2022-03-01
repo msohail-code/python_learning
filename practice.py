dacfeee68f0b97c2410df1259a7c83fb2067a0d2
Sohail <tutstricks@gmail.com>
2022-03-01 14:28:40 +0500

    initial version

---
+++ practice.py
@@ -0,0 +1,38 @@
+# single line
+
+'''
+multiline comment
+'''
+
+
+var = int(input("Enter something:")) # this will only take string input
+
+#print(2+var)
+
+var2 = 3;
+# Conditionals
+'''
+if var == 4:
+    print("This number is 4")
+elif var == 5:
+    print(var)
+else:
+    print("THis is something else")
+
+'''
+# operators
+
+# == comparison
+# != not equal
+# and, or
+# (v1==v2)?"ss":else"
+
+var3 = 3 if (var==var2) else 5
+print(var3)
+'''
+if var != 3 and var2 == 3:
+    print("number is not 3")
+
+else:
+    print(var)
+'''
